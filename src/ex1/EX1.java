bb92ea14a0258b037b1b74e0d2c08fda137c5542
notemct <note_comgear_x@hotmail.co.th>
2014-05-30 13:53:23 +0700

    EX1

---
+++ src/ex1/EX1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ex1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class EX1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is my first try");
+        System.out.println("at programming in java");
+        System.out.println("It is fun");
+    
+    }
+    
+}
